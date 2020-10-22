ac4b009c1fabb48a766aa2c04e887f0b4e218182
venkatesh <srivenkatesh17041999@gmail.com>
2020-10-22 14:29:57 +0530

    user registration mobile number validation

--- pattern2.sh
+++ pattern2.sh
@@ -1,18 +1,36 @@
 echo "welcome to user registration"
+shopt -s extglob
 read first name
-regex="^[A-Z]+[a-z]{2,}"
-if [[ $name =~ $regex ]]
+regex1="^[A-Z][a-z]{2,}"
+if [[ $name =~ $regex1 ]]
 then 
 echo "valid"
 else
 echo "invalid"
 fi
 read last name
-regex="^[A-Z][a-z]{2,}"
-if [[ $name =~ $regex ]]
+regex2="^[A-Z][a-z]{2,}"
+if [[ $name =~ $regex2 ]]
 then
 echo "valid"
 else
 echo "invalid"
 fi
+read email
+regex3="^[abc]+[A-Za-z]{2}+?[@]+[bl.co][.][a-zA-Z]$"
+if [[ $email =~ $regex3 ]]
+then
+echo "valid"
+else
+echo "invalid"
+fi
+read mobile
+regex4="[0-9]{2,5}[[:space:]][[:digit:]]{10}$"
+if [[ $mobile =~ $regex4 ]]
+then
+echo "valid"
+else
+echo "invalid"
+fi
+
 
